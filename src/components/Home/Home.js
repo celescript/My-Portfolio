db4d3e4e5f370efd20eeef7bf173ed7993d8e393
celestef00 <celestefernandez00@gmail.com>
2022-03-25 18:19:03 -0300

    home ui changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,9 +5,11 @@ import styles from './Home.module.css'
 
 const Home = () => {
   return (
-    <div className={styles.home__container}>
-    
-
+    <div className={styles.home__container} id='#Home'>
+      <div className={styles.hometitle__container}>
+        <h1 className={styles.home__title}> CELESTE FERNANDEZ </h1>
+        <h2 className={styles.home__subtitle} >Front-End Developer</h2>
+      </div>
     </div>
   )
 }
