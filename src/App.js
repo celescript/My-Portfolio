9cc42384ea12b154ba382534dd525b5ced647582
celestef00 <celestefernandez00@gmail.com>
2022-04-06 16:24:40 -0300

    Contact ui

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar/Navbar'
 import Home from './components/Home/Home'
 import Work from './components/Work/Work'
 import About from './components/About/About'
+import Contact from './components/Contact/Contact'
 
 const App = () => {
   return (<>
@@ -11,6 +12,7 @@ const App = () => {
     <Home />
     <Work />
     <About />
+    <Contact/>
 
     
 
---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,91 @@
+import React from 'react'
+
+import styles from './Contact.module.css'
+
+import image from '../../assets/images/AboutIcons/Download.svg'
+
+
+
+const Contact = () => {
+
+  const AboutData = [
+    {
+      id: 1,
+      number: styles.odd,
+      name: 'github',
+      image: 'Github.svg',
+      text: 'celesf00',
+      link: 'https://github.com/celestef00'
+
+    },
+    {
+      id: 2,
+      number: styles.even,
+      name: 'linkedin',
+      image: 'Linkedin.svg',
+      text: 'Celeste Fernandez',
+      link: 'https://www.linkedin.com/in/celestefernandez00/?locale=en_US'
+
+    },
+    {
+      id: 3,
+      number: styles.odd,
+      name: 'mail',
+      image: 'Mail.svg',
+      text: 'celestefernandez00@gmail.com',
+      link: 'mailto:celestefernandez00@gmail.com'
+
+    }
+
+
+  ]
+
+  return (
+    <div className={styles.contact__container} id='Contact'>
+      
+      <h1 className={styles.contact__title}>
+        Let's get in touch...
+      </h1>
+
+      <div className={styles.contact__body}>
+      {
+        AboutData.map((data)=> {
+          return(
+            
+
+            <a key={data.id} className={` ${styles.contact__btn} ${data.number} ${styles.link}`} href={data.link} target="_blank" rel="noreferrer"> 
+  
+              <img alt={`${data.name} icon`} className={styles.image} src={require(`../../assets/images/AboutIcons/${data.image}`)}></img>
+
+              <p className={styles.text}>{data.text}</p>
+  
+            </a>
+  
+            
+  
+          
+
+          )
+        })
+
+      }
+
+        <div className={`${styles.contact__btn} ${styles.even} `}>
+          <a className={styles.link} href={'/CELESTEFERNANDEZ-CV.pdf'} target="_blank" rel="noreferrer"> 
+
+            <img alt='download icon' className={styles.image} src={image}></img>
+
+          </a>
+
+          <p className={styles.text}>Download my CV</p>
+
+        </div>
+
+      </div>
+
+      
+    </div>
+  )
+}
+
+export default Contact
